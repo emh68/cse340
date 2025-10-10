05ba02c5c00e27010c6e27b18fbbb5f056310bf6
Eli Hansen <elihansen@yahoo.com>
2025-10-10 12:48:24 -0600

    Updated to follow MVC implementation and add vehicle classifications to navbar. Adjusted CSS for classifications to create vehicle cards and card styling.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -22,12 +23,13 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.get("/", baseController.buildHome)
+app.use(static)
 
 //Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
