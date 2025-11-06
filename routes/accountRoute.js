e39e8496b21020c80282e3f7ca833c6cdb6d7781
Eli Hansen <elihansen@yahoo.com>
2025-11-06 15:44:52 -0700

    Added account-validation.js file and code to sanitize

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,14 +3,17 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation');
+
 
 // Login view route
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Registration view route
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Route to update database with registration data
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post("/register", regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount));
+
 
 module.exports = router;
