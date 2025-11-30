f6b72ef48142e217143791a60f1c26981c03533d
Eli Hansen <elihansen@yahoo.com>
2025-11-29 20:49:00 -0700

    removed unnecessary js code for button disable

--- public/js/inv-update.js
+++
@@ -1,5 +0,0 @@
-const form = document.querySelector("#updateForm")
-const updateBtn = form.querySelector("button[type='submit']")
-form.addEventListener("change", function () {
-    updateBtn.removeAttribute("disabled")
-})
